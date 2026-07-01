0a64191a3fc9ab0a51e064deaf2a04a5710a934c
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-07-01 15:28:45 -0600

    feat(W-23195021): add RT flag assertion to validateUserAgent and assertRevokeAndRefreshWorks

--- native/NativeSampleApps/AuthFlowTester/src/androidTest/java/com/salesforce/samples/authflowtester/testUtility/AuthFlowTest.kt
+++ native/NativeSampleApps/AuthFlowTester/src/androidTest/java/com/salesforce/samples/authflowtester/testUtility/AuthFlowTest.kt
@@ -441,7 +441,10 @@ abstract class AuthFlowTest {
         app.validateApiRequest()
     }
 
-    fun assertRevokeAndRefreshWorks(isRtr: Boolean) {
+    fun assertRevokeAndRefreshWorks(
+        isRtr: Boolean,
+        knownLoginHostConfig: KnownLoginHostConfig = REGULAR_AUTH,
+    ) {
         val (preAccessToken, preRefreshToken) = app.getTokens()
         app.revokeAccessToken()
         app.validateApiRequest()
@@ -451,6 +454,7 @@ abstract class AuthFlowTest {
 
         if (isRtr) {
             assert(preRefreshToken != postRefreshToken) { "Refresh token should have rotated (RTR app)" }
+            app.validateUserAgent(knownLoginHostConfig = knownLoginHostConfig, isRtr = true)
         } else {
             assert(preRefreshToken == postRefreshToken) { "Refresh token should not have changed (non-RTR app)" }
         }
